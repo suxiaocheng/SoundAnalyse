de73cf03c8878e18c85c6620b6b8f83fac577c18
suxiaocheng <suxiaocheng2010@gmail.com>
2015-12-09 23:03:06 +0800

    add the ui

--- app/src/main/java/com/silicongo/george/soundanalyse/MainActivity.java
+++ app/src/main/java/com/silicongo/george/soundanalyse/MainActivity.java
@@ -1,13 +1,99 @@
 package com.silicongo.george.soundanalyse;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecordJob.OnFragmentInteractionListener {
+
+    private static final String TAG = "MainActivity";
+
+    private SoundAnalysePagerAdapter mDemoCollectionPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ViewPager and its adapters use support library
+        // fragments, so use getSupportFragmentManager.
+        mDemoCollectionPagerAdapter = new SoundAnalysePagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mDemoCollectionPagerAdapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // Since this is an object collection, use a FragmentStatePagerAdapter,
+// and NOT a FragmentPagerAdapter.
+    public class SoundAnalysePagerAdapter extends FragmentStatePagerAdapter {
+
+        Fragment[] emmcUtilsFragment;
+
+        public SoundAnalysePagerAdapter(FragmentManager fm) {
+            super(fm);
+
+            emmcUtilsFragment = new Fragment[]{new RecordJob()};
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = emmcUtilsFragment[i];
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return emmcUtilsFragment.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "OBJECT " + (position + 1);
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
